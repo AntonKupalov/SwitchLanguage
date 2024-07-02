09a94f65d932afee8f207494ffb68150593bda89
AntonKupalov <137334848+AntonKupalov@users.noreply.github.com>
2024-07-02 12:38:42 -0500

    First commit

---
+++ SwitchLanguage/SwitchLanguage/CustomMiddleware/CustomLocalizationMiddleware.cs
@@ -0,0 +1,34 @@
+using System.Globalization;
+
+namespace SwitchLanguage.CustomMiddleware;
+
+public class CustomLocalizationMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CustomLocalizationMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        CultureInfo culture;
+        var localFromCulture = context.Request.Cookies["langues"];
+        if (localFromCulture == null)
+        {
+            string acceptLanguage = context.Request.Headers.AcceptLanguage;
+            var locale = acceptLanguage.Substring(0, 2);
+            culture = new CultureInfo(locale);
+        }
+        else
+        {
+            culture = new CultureInfo(localFromCulture);
+        }
+
+        Thread.CurrentThread.CurrentCulture = culture;
+        Thread.CurrentThread.CurrentUICulture = culture;
+
+        await _next.Invoke(context);
+    }
+}
---
+++ SwitchLanguage/SwitchLanguage/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SwitchLanguage.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
